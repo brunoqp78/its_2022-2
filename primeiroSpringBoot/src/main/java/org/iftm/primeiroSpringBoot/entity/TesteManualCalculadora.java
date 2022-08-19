ce6193aaf6e856e411f81c6f62fbf91c016a8210
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2022-08-18 22:20:21 -0300

    Classes Calculadora e Teste Manual

---
+++ primeiroSpringBoot/src/main/java/org/iftm/primeiroSpringBoot/entity/TesteManualCalculadora.java
@@ -0,0 +1,27 @@
+package org.iftm.primeiroSpringBoot.entity;
+
+
+public class TesteManualCalculadora {
+
+	public static void main(String[] args) {
+		//1. parte: definir cenário de teste
+		//instanciar os objetos a serem testado
+		Calculadora calculadora = new Calculadora();
+		//definir as variáveis de entrada e resultado esperado
+		//entradas necessárias para o teste
+		double entradaNumero1 = 10;
+		double entradaNumero2 = 5;
+		//saída esperada do teste
+		double resultadoEsperado = 15;
+		//2. parte: executar a função a ser testada
+		calculadora.somar(entradaNumero1, entradaNumero2);
+		double resultadoObtido = calculadora.getResultado();
+		//3. parte: verificar o resultado
+		if (resultadoEsperado == resultadoObtido) {
+			System.out.println("Passou no teste");
+		}else {
+			System.out.println("Não passou no teste");
+		}
+	}
+
+}
