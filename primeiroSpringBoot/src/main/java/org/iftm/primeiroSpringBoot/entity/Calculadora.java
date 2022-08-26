2606a672882b52ade7c970af44833cba4bdb96ec
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2022-08-25 21:18:39 -0300

    Criação da classe CalculadoraTest e do teste da soma de dois inteiros

--- primeiroSpringBoot/src/main/java/org/iftm/primeiroSpringBoot/entity/Calculadora.java
+++ primeiroSpringBoot/src/main/java/org/iftm/primeiroSpringBoot/entity/Calculadora.java
@@ -4,7 +4,7 @@ public class Calculadora {
 	private int resultado;
 
 	public void somar(int num1, int num2) {
-		resultado = num1 - num2;
+		resultado = num1 + num2;
 	}
 
 	public void subtrair(int num1, int num2) {
